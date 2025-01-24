1846b586fcf354da6f163c1a79b79a21baddaf0d
drbolzan <90543973+drbolzan@users.noreply.github.com>
2025-01-24 11:24:46 -0800

    Update predict_intra_cell_line_all.py

--- predict_intra_cell_line_all.py
+++ predict_intra_cell_line_all.py
@@ -7,9 +7,9 @@ cell_lines = ['H1', 'H9', 'HCT116', 'mESC', 'mNPC']
 
 for cell_line in cell_lines:
     if 'm' in cell_line:
-        train_chroms = (' ').join([str(i) for i in range(1,20) if i != 6])
+        train_chroms = (' ').join([str(i) for i in range(1,20) if i != 6 and i != 9])
     else:
-        train_chroms = (' ').join([str(i) for i in range(1,23) if i != 6])
+        train_chroms = (' ').join([str(i) for i in range(1,23) if i != 6 and i != 9])
     command = f"python -u predict_intra_cell_line.py " \
               f"--train_features_file ./data/{cell_line}_features.npz " \
               f"--test_features_file ./data/{cell_line}_features.npz " \
